a911f285123fe9e564926a224b5679e7f31758c3
qbLedger <qb_ledger@quarkslab.com>
2021-11-29 06:12:02 -0800

    Update fuzzer

--- fuzzing/fuzz_app_dock.c
+++ fuzzing/fuzz_app_dock.c
@@ -3,6 +3,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
+static char PARSER_KEY[16384];
+static char PARSER_VALUE[16384];
+
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     parser_tx_t txObj;
     parser_context_t ctx;
@@ -12,5 +15,41 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     if (rc != parser_ok) {
         return 0;
     }
-    return 0;
+    rc = parser_validate(&ctx);
+    if (rc != parser_ok) {
+        return 0;
+    }
+
+    uint8_t num_items;
+    rc = parser_getNumItems(&ctx, &num_items);
+    if (rc != parser_ok) {
+        fprintf(stderr,
+                "error in parser_getNumItems: %s\n",
+                parser_getErrorDescription(rc));
+    }
+
+
+
+    for (uint8_t i = 0; i < num_items; i += 1) {
+        uint8_t page_idx = 0;
+        uint8_t page_count = 1;
+        while (page_idx < page_count) {
+            rc = parser_getItem(&ctx, i,
+                                PARSER_KEY, sizeof(PARSER_KEY),
+                                PARSER_VALUE, sizeof(PARSER_VALUE),
+                                page_idx, &page_count);
+
+
+
+            if (rc != parser_ok) {
+                fprintf(stderr,
+                        "error getting item %u at page index %u: %s\n",
+                        (unsigned)i,
+                        (unsigned)page_idx,
+                        parser_getErrorDescription(rc));
+            }
+
+            page_idx += 1;
+        }
+    }
 }
