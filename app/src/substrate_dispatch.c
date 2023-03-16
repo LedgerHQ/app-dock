ab4d0fdbfb5f3c848637b7d7d94a045a8d1750e9
lovesh <lovesh.bond@gmail.com>
2023-03-17 03:29:26 +0530

    Add pending version upgrades, remove a mistakenly committed file and add `build` directory to .gitignore
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- app/src/substrate_dispatch.c
+++ app/src/substrate_dispatch.c
@@ -28,6 +28,8 @@ parser_error_t _readMethod(
     switch (c->tx_obj->transactionVersion) {
     case 1:
         return _readMethod_V1(c, moduleIdx, callIdx, &method->V1);
+    case 2:
+        return _readMethod_V2(c, moduleIdx, callIdx, &method->V2);
     default:
         return parser_tx_version_not_supported;
     }
@@ -38,6 +40,8 @@ parser_error_t _getMethod_NumItems(uint32_t transactionVersion, uint8_t moduleId
     switch (transactionVersion) {
     case 1:
         return _getMethod_NumItems_V1(moduleIdx, callIdx, numItems);
+    case 2:
+        return _getMethod_NumItems_V2(moduleIdx, callIdx, numItems);
     default:
         // TODO: This isn't correct. A mutable argument should be expected and error value should be checked of this function
         return parser_tx_version_not_supported;
@@ -49,6 +53,8 @@ const char* _getMethod_ModuleName(uint32_t transactionVersion, uint8_t moduleIdx
     switch (transactionVersion) {
     case 1:
         return _getMethod_ModuleName_V1(moduleIdx);
+    case 2:
+        return _getMethod_ModuleName_V2(moduleIdx);
     default:
         return NULL;
     }
@@ -59,6 +65,8 @@ const char* _getMethod_Name(uint32_t transactionVersion, uint8_t moduleIdx, uint
     switch (transactionVersion) {
     case 1:
         return _getMethod_Name_V1(moduleIdx, callIdx);
+    case 2:
+        return _getMethod_Name_V2(moduleIdx, callIdx);
     default:
         return 0;
     }
@@ -69,6 +77,8 @@ const char* _getMethod_ItemName(uint32_t transactionVersion, uint8_t moduleIdx, 
     switch (transactionVersion) {
     case 1:
         return _getMethod_ItemName_V1(moduleIdx, callIdx, itemIdx);
+    case 2:
+        return _getMethod_ItemName_V2(moduleIdx, callIdx, itemIdx);
     default:
         return NULL;
     }
@@ -82,6 +92,9 @@ parser_error_t _getMethod_ItemValue(uint32_t transactionVersion, pd_Method_t* m,
     case 1:
         return _getMethod_ItemValue_V1(&m->V1, moduleIdx, callIdx, itemIdx, outValue,
             outValueLen, pageIdx, pageCount);
+    case 2:
+        return _getMethod_ItemValue_V2(&m->V2, moduleIdx, callIdx, itemIdx, outValue,
+                                       outValueLen, pageIdx, pageCount);
     default:
         return parser_tx_version_not_supported;
     }
@@ -92,6 +105,8 @@ bool _getMethod_ItemIsExpert(uint32_t transactionVersion, uint8_t moduleIdx, uin
     switch (transactionVersion) {
     case 1:
         return _getMethod_ItemIsExpert_V1(moduleIdx, callIdx, itemIdx);
+    case 2:
+        return _getMethod_ItemIsExpert_V2(moduleIdx, callIdx, itemIdx);
     default:
         return false;
     }
@@ -102,6 +117,8 @@ bool _getMethod_IsNestingSupported(uint32_t transactionVersion, uint8_t moduleId
     switch (transactionVersion) {
     case 1:
         return _getMethod_IsNestingSupported_V1(moduleIdx, callIdx);
+    case 2:
+        return _getMethod_IsNestingSupported_V2(moduleIdx, callIdx);
     default:
         return false;
     }
